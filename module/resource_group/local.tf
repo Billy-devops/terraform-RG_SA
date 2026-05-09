24c98cc07f5435387dd89b8fccc7d5b616d3e05c
Shivam Prajapati <tonystarkt225@gmail.com>
2026-05-09 16:10:38 +0530

    Delete module directory

--- module/resource_group/local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  common_tags = {
-    managed_by = "terraform"
-    project    = var.prefix
-  }
-}
-# 👉 temporary variable (sirf Terraform ke andar use hota hai)
-# 👉 reuse ke liye use hota hai
